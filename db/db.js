d9fa2a944cb5800aa904facba0db6e8157269e05
gajanannagare <gajanannagare16@gmail.com>
2025-08-14 18:04:35 +0530

    Added config file

--- db/db.js
+++ db/db.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.CONNECTION_STRING);
